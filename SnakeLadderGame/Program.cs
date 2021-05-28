aaa10f30147966210811ee0b781173e6cb324eed
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-28 11:15:22 +0530

    [sohail] Add: welcome message for Snake and Ladder Game

---
+++ SnakeLadderGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake n Ladder Game");
+        }
+    }
+}
