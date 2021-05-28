369600c0e5918c0489f4b7f597be0b6079d598fb
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-28 11:45:01 +0530

    [sohail] Add: Dice to game to genrate random number

--- SnakeLadderGame/SnakeLadder.cs
+++ SnakeLadderGame/SnakeLadder.cs
@@ -15,12 +15,22 @@ namespace SnakeLadderGame
             //initialize player
             playerOne = position;
             Console.WriteLine($"player One position is {playerOne}");
+            playerOne = RollDie();
+            Console.WriteLine($"player One rolls die and get position {playerOne}");
         }
-
+        public int RollDie()
+        {
+            //Roll die to produce random number between 1-6
+            Random random = new Random();
+            int dice = random.Next(1,7);
+            return dice;
+        }
+        //start of game 
         public void Board()
         {
             //at the start
             Start();
+            
         }
 
     }
